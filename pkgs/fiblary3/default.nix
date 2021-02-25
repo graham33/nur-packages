31f6144275b63dfbc1fc9355b69871d3031e8c53
Graham Bennett <graham@grahambennett.org>
2021-02-25 00:24:06 +0000

    Update fiblary

--- pkgs/fiblary3/default.nix
+++ pkgs/fiblary3/default.nix
@@ -1,14 +1,19 @@
 { lib
 , buildPythonPackage
 , isPy3k
-, fetchPypi
+, fetchFromGitHub
+, fixtures
 , jsonpatch
+, mock
 , netaddr
 , prettytable
+, pytestCheckHook
 , python-dateutil
 , requests
+, requests-mock
 , six
 , sphinx
+, testtools
 }:
 
 buildPythonPackage rec {
@@ -16,9 +21,12 @@ buildPythonPackage rec {
   version = "0.1.8";
   disabled = !isPy3k;
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "ab666088d1996e1cc510ff91c9ff00ac14c7304d327d478ad948b3ea0c27668e";
+  src = fetchFromGitHub {
+    # TODO: fix
+    owner = "graham33";
+    repo = "fiblary";
+    rev = "0d04befbaacbe56a3c59ca4fb862732639abebeb";
+    sha256 = "1h332f68mq89qshqwn781wzz5mg2359j0636j2qr8fh3jw10kkzp";
   };
 
   nativeBuildInputs = [
@@ -33,6 +41,16 @@ buildPythonPackage rec {
     requests
     six
   ];
+
+  checkInputs = [
+    fixtures
+    mock
+    pytestCheckHook
+    requests-mock
+    testtools
+  ];
+
+  pythonImportsCheck = [ "fiblary3" ];
 
   meta = with lib; {
     homepage = "https://github.com/pbalogh77/fiblary";
