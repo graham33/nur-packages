2a6ab48f242d0773de775b7e1140fa36a90019cb
Graham Bennett <graham@grahambennett.org>
2021-11-06 18:10:52 +0000

    homeassistant-stubs: bump to 2021.10.6

--- pkgs/homeassistant-stubs/default.nix
+++ pkgs/homeassistant-stubs/default.nix
@@ -7,14 +7,14 @@
 
 buildPythonPackage rec {
   pname = "homeassistant-stubs";
-  version = "2021.10.4";
+  version = "2021.10.6";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "KapJI";
     repo = pname;
     rev = "${version}";
-    sha256 = "06y2595pw3zb3hspfqw58k0g42r6jkrl46c8d09d5zx9f5ms0fqn";
+    sha256 = "1ir270v38i9ws60qmj2zf6w9q358zvx1qdpq07ipizqm645hjwx4";
   };
 
   nativeBuildInputs = [
