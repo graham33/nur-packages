26d665bbdb427027b02e3dc891d38f36e8578b4e
Graham Bennett <graham@grahambennett.org>
2023-11-12 15:09:56 +0000

    Upgrade HA deps

--- pkgs/pytest-homeassistant-custom-component/default.nix
+++ pkgs/pytest-homeassistant-custom-component/default.nix
@@ -36,14 +36,14 @@
 
 buildPythonPackage rec {
   pname = "pytest-homeassistant-custom-component";
-  version = "0.13.66";
+  version = "0.13.76";
   disabled = !isPy3k || isPy37;
 
   src = fetchFromGitHub {
     owner = "MatthewFlamm";
     repo = pname;
     rev = version;
-    sha256 = "1p8mfmgcca88dc8dzj64zyssbxjynsb05h733a54s2755zr0r6nl";
+    sha256 = "1sapp9xi2jglzbs4ngi83zbi818shfgw084dxzwbdyfqg6081d5r";
   };
 
   postPatch = ''
